b9304dbb9609e7c6f51cc885003acbf6d9e9e980
diogods7 <diogosalgueiro16@gmail.com>
2026-02-26 00:17:25 -0300

    oiu

--- src/dashboard.ts
+++ src/dashboard.ts
@@ -1,10 +1,19 @@
 import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 import { ExpressAdapter } from "@bull-board/express";
+import { emailQueue } from "./queue";
 
 export function setupBullBoard() {
   const serverAdapter = new ExpressAdapter()
 
   serverAdapter.setBasePath("/admin/queues");
 
-  createBullBoard 
+  createBullBoard({
+    queues: [
+      new BullMQAdapter(emailQueue)
+    ],
+    serverAdapter
+  })
+
+  return serverAdapter
 }
