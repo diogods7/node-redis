b189493ac339345655748126e0cb52fe63df10ba
diogods7 <diogosalgueiro16@gmail.com>
2026-02-25 22:43:05 -0300

    afl

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,15 @@
 import express from "express";
+import { z } from "zod";
 
 const app = express()
 app.use(express.json())
+
+const schema = z.object({
+  to: z.email(),
+  subject: z.string().min(1),
+  body: z.string().min(1),
+  requestId: z.string().min(2) 
+})
 
 const port = Number(process.env.PORT ?? 3000)
 app.listen(port, () => console.log(`API on port: ${port}`))
